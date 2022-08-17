6f69c18efd83512bf9834612456891ccfc17a649
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-08-16 23:54:17 -0500

    cargar eventos de la base de datos

--- app/Http/Controllers/FullCalendarController.php
+++ app/Http/Controllers/FullCalendarController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class FullCalendarController extends Controller
 {
-    //
+    public function index(Request $request){
+        if($request->ajax()) {
+
+            $data = Event::whereDate('start', '>=', $request->start)
+                ->whereDate('end',   '<=', $request->end)
+                ->get(['id', 'title', 'start', 'end']);
+
+            return response()->json($data);
+        }
+        return view('system.index');
+    }
 }
